b95882c247b000ec7da364596709361df906ebfb
Nur Ridho A <abdurrahmansyah234@gmail.com>
2020-07-24 17:49:01 +0700

    Commit code

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  qrData = 'http://instagram.com/';
+  scannedCode = null;
+  elementType: 'url' | 'canvas' | 'img' = 'canvas';
+
+  constructor(private barcodeScanner: BarcodeScanner,
+    private base64ToGallery: Base64ToGallery,
+    private toastCtrl: ToastController) { }
+
+  scanCode() {
+    this.barcodeScanner.scan().then(
+      barcodeData => {
+        this.scannedCode = barcodeData.text;
+      }
+    );
+  }
+
+  downloadQR() {
+    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+    const imageData = canvas.toDataURL('image/jpeg').toString();
+    console.log('data: ', imageData);
+
+    let data = imageData.split(',')[1];
+
+    this.base64ToGallery.base64ToGallery(data,
+      { prefix: '_img', mediaScanner: true })
+      .then(res => {
+        let toast = this.toastCtrl.create({
+          header: 'QR Code daved in your Photolibrary'
+        });
+      }, err => console.log('error: ', err)
+      );
+  }
+}
